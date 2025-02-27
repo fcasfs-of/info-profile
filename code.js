15c97bef08a9a40e1f75ec5d5a757cfefb0b5f9e
FCAS <94762834+fcasfs-of@users.noreply.github.com>
2025-02-27 09:38:13 -0300

    Update code.js

--- code.js
+++ code.js
@@ -3,13 +3,13 @@ let btn = document.querySelector(".darkmode");
 btn.addEventListener("click", () => {
   if (btn.innerHTML === `<i class="fas fa-moon"></i>`) {
     btn.innerHTML = `<i class="fas fa-sun"></i>`;
-    btn.style.color = "orange";
-    btn.style.backgroundColor = "#111";
+    btn.style.color = "#000";
+    btn.style.backgroundColor = "#fff";
     document.body.classList.add("darkmode");
   } else {
     btn.innerHTML = `<i class="fas fa-moon"></i>`;
-    btn.style.color = "#000";
-    btn.style.backgroundColor = "#fff";
+   btn.style.color = "orange";
+    btn.style.backgroundColor = "#111";
     document.body.classList.remove("darkmode");
   }
 });

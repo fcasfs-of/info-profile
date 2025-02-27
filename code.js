2b2bce05d7a8ef702f7649e4caca172f740bc7e0
FCAS <94762834+fcasfs-of@users.noreply.github.com>
2025-02-27 08:59:03 -0300

    Update code.js

--- code.js
+++ code.js
@@ -4,12 +4,12 @@ btn.addEventListener("click", () => {
   if (btn.innerHTML === `<i class="fas fa-moon"></i>`) {
     btn.innerHTML = `<i class="fas fa-sun"></i>`;
     btn.style.color = "orange";
-    btn.style.backgroundColor = "rgb(43, 43, 43)";
+    btn.style.backgroundColor = "#fff";
     document.body.classList.add("darkmode");
   } else {
     btn.innerHTML = `<i class="fas fa-moon"></i>`;
     btn.style.color = "black";
-    btn.style.backgroundColor = "white";
+    btn.style.backgroundColor = "#111";
     document.body.classList.remove("darkmode");
   }
 });

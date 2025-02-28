2c0950c175ae668e4df6a4ec5c9c679d80b6f922
FCAS <94762834+fcasfs-of@users.noreply.github.com>
2025-02-28 10:44:34 -0300

    Update core.js

--- core.js
+++ core.js
@@ -1,1 +1,17 @@
 
+  function mode_chanhe(btn) {
+  if (btn.innerHTML === `<i class="fas fa-moon"></i>`) {
+    btn.innerHTML = `<i class="fas fa-sun"></i>`;
+    btn.style.color = "#000";
+    btn.style.backgroundColor = "#fff";
+    document.body.classList.add("darkmode");
+  } else {
+    btn.innerHTML = `<i class="fas fa-moon"></i>`;
+   btn.style.color = "orange";
+    btn.style.backgroundColor = "#111";
+    document.body.classList.remove("darkmode");
+  }
+}
+
+
+

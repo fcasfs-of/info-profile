546a3cd44a722825386eee774a1b4e1ffe8daf5f
FCAS <94762834+fcasfs-of@users.noreply.github.com>
2025-02-28 10:14:28 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,18 +1,5 @@
 let btn = document.querySelector(".cbtn > .darkmode");
 
-
-var myNodelist = document.getElementsByTagName("LI");
-var is;
-for (is = 0; is < myNodelist.length; is++) {
- 
-if(myNodelist[is].classList=="link"){
- if(myNodelist[is].getElementsByTagName("a")[0]){    myNodelist[is].style.cursor = "pointer";  myNodelist[is].onclick = function(){   myNodelist[is].getElementsByTagName("a")[0].click();    };   } 
-}
- 
-}
-
-
-
 btn.addEventListener("click", () => {
   if (btn.innerHTML === `<i class="fas fa-moon"></i>`) {
     btn.innerHTML = `<i class="fas fa-sun"></i>`;
@@ -26,4 +13,19 @@ btn.addEventListener("click", () => {
     document.body.classList.remove("darkmode");
   }
 });
+
+
+
+var myNodeflist = document.querySelector("ul");
+for (var ids = 0; ids < myNodeflist.length; ids++) {
+var myNodelist = myNodeflist[ids].getElementsByTagName("LI");
+var is;
+for (is = 0; is < myNodelist.length; is++) {
+if(myNodelist[is].classList=="link"){
+ if(myNodelist[is].getElementsByTagName("a")[0]){    myNodelist[is].style.cursor = "pointer";  myNodelist[is].onclick = function(){   myNodelist[is].getElementsByTagName("a")[0].click();    };   } 
+}
+}
+}
+
+
 

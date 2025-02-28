d0d3b6b3f02f175e04651d985080a052f922741a
FCAS <94762834+fcasfs-of@users.noreply.github.com>
2025-02-28 10:48:03 -0300

    Update code.js

--- code.js
+++ code.js
@@ -5,12 +5,16 @@ for (var ids = 0; ids < myNodeflist.length; ids++) {
 var myNodelist = myNodeflist[ids].getElementsByTagName("LI");
 var is;
   
-function myNodeflist_od(is){    myNodelist[is].getElementsByTagName("a")[0].click;   }  
+function myNodeflist_od(is,mm){    myNodelist[is].getElementsByTagName(mm)[0].click();   }  
   
 for (is = 0; is < myNodelist.length; is++) {
 if(myNodelist[is].classList=="link"){
- if(myNodelist[is].getElementsByTagName("a")[0]){     myNodelist[is].style.cursor = "pointer";  myNodelist[is].onclick = myNodeflist_od(is);   } 
+ if(myNodelist[is].getElementsByTagName("a")[0]){     myNodelist[is].style.cursor = "pointer";  myNodelist[is].onclick = function(){  myNodeflist_od(is, "a");  };   } 
 }
+else if(myNodelist[is].classList=="cbtn"){
+ if(myNodelist[is].getElementsByTagName("button")[0]){     myNodelist[is].style.cursor = "pointer";  myNodelist[is].onclick = function(){  myNodeflist_od(is, "button");  };   } 
+}
+  
 }
 }
 

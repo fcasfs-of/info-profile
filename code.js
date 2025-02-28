03c900c79e83cba74ac6a3c3d54e5a30805e1fb5
FCAS <94762834+fcasfs-of@users.noreply.github.com>
2025-02-28 10:52:32 -0300

    Update code.js

--- code.js
+++ code.js
@@ -5,14 +5,19 @@ for (var ids = 0; ids < myNodeflist.length; ids++) {
 var myNodelist = myNodeflist[ids].getElementsByTagName("LI");
 var is;
   
-function myNodeflist_od(is,mm){    myNodelist[is].getElementsByTagName(mm)[0].click();   }  
   
 for (is = 0; is < myNodelist.length; is++) {
+  
 if(myNodelist[is].classList=="link"){
- if(myNodelist[is].getElementsByTagName("a")[0]){     myNodelist[is].style.cursor = "pointer";  myNodelist[is].onclick = function(){  myNodeflist_od(is, "a");  };   } 
+ if(myNodelist[is].getElementsByTagName("a")[0]){     myNodelist[is].style.cursor = "pointer";  myNodelist[is].onclick = function(){    this.getElementsByTagName("a")[0].click();   };   } 
 }
+  
 else if(myNodelist[is].classList=="cbtn"){
- if(myNodelist[is].getElementsByTagName("button")[0]){     myNodelist[is].style.cursor = "pointer";  myNodelist[is].onclick = function(){  myNodeflist_od(is, "button");  };   } 
+ if(myNodelist[is].getElementsByTagName("button")[0]){     myNodelist[is].style.cursor = "pointer";  myNodelist[is].onclick = function(){    this.getElementsByTagName("button")[0].click();   };   } 
+}
+  
+else if(myNodelist[is].classList=="zlink"){
+ if(myNodelist[is].getElementsByTagName("button")[0]){     myNodelist[is].style.cursor = "pointer";  myNodelist[is].onclick = function(){    this.getElementsByTagName("button")[0].click();   };   } 
 }
   
 }

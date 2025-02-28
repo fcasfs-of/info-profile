d7f0dad2a543bbe2b5841530f49b0684b49a3b9a
FCAS <94762834+fcasfs-of@users.noreply.github.com>
2025-02-28 09:10:06 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,12 +1,20 @@
 let btn = document.querySelector(".darkmode");
 
-let menu_links = document.getElementsByClassName("links");
+let menu_lifnks = document.querySelector(".zlink");
+let menu_links = document.querySelector(".link");
  for (id = 0; id < menu_links.length; id++) {
    if(menu_links[id].getElementsByTagName("a")[0]){
     tabcontent[id].style.cursor = "pointer";
-tabcontent[id].addEventListener("click", function(){   menu_links[id].getElementsByTagName("li")[0].click();    });
+tabcontent[id].addEventListener("click", function(){   menu_links[id].getElementsByTagName("a")[0].click();    });
    }
   }
+for (idd = 0; idd < menu_lifnks.length; idd++) {
+   if(menu_lifnks[idd].getElementsByTagName("a")[0]){
+    menu_lifnks[idd].style.cursor = "pointer";
+menu_lifnks[idd].addEventListener("click", function(){   menu_lifnks[idd].getElementsByTagName("a")[0].click();    });
+   }
+  }
+
 
 btn.addEventListener("click", () => {
   if (btn.innerHTML === `<i class="fas fa-moon"></i>`) {

aaf63457b02a195dd7db3dd535f4a0406ee3c41b
FCAS <94762834+fcasfs-of@users.noreply.github.com>
2025-02-28 10:44:21 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,17 +1,3 @@
-  function mode_chanhe(btn) {
-  if (btn.innerHTML === `<i class="fas fa-moon"></i>`) {
-    btn.innerHTML = `<i class="fas fa-sun"></i>`;
-    btn.style.color = "#000";
-    btn.style.backgroundColor = "#fff";
-    document.body.classList.add("darkmode");
-  } else {
-    btn.innerHTML = `<i class="fas fa-moon"></i>`;
-   btn.style.color = "orange";
-    btn.style.backgroundColor = "#111";
-    document.body.classList.remove("darkmode");
-  }
-}
-
 
 
 var myNodeflist = document.getElementsByTagName("UL");

4fa7238e87681a9d85737d94ead92f6c93f3a91b
FCAS <94762834+fcasfs-of@users.noreply.github.com>
2025-02-28 09:20:54 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,10 +1,10 @@
 let btn = document.querySelector(".darkmode");
 
 
-let htr_list= document.querySelector(".navbar__ul");
+let htr_list= document.getElementsByClassName("navbar__ul");
 
 let menu_lifnks = document.getElementsByClassName("zlink");
-let menu_links = htr_list.getElementsByClassName("link");
+let menu_links = htr_list.querySelector(".link");
  for (id = 0; id < menu_links.length; id++) {
    if(menu_links[id].getElementsByTagName("a")[0]){
     tabcontent[id].style.cursor = "pointer";

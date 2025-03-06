80aa4236a7be5c9b3001341146f6e303bf3e8ad8
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-03-06 15:55:09 -0300

    Update code.js

--- scripts/code.js
+++ scripts/code.js
@@ -28,7 +28,7 @@ else if(myNodelist[is].classList=="zlink"){
 
 if (localStorage.getItem("ldark")!="" && localStorage.getItem("ldark")!=null){
 if (localStorage.getItem("ldark")=="yes"){
-  const changedark = document.getElementsByClassName("btndk");
+  const changedark = document.getElementsById("btndk");
   if(changedark){   changedark.click();  }
 }
 }

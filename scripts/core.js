61ee74a5be1a4c6ddaf6ff5bc3cdd8568afaf3ee
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-03-06 16:12:35 -0300

    Update core.js

--- scripts/core.js
+++ scripts/core.js
@@ -16,5 +16,25 @@
 }
 
 
+function setmode_chanhe(btn,nn) {
+  if (nn === `yes`) {
+    btn.innerHTML = `<i class="fas fa-sun"></i>`;
+    btn.style.color = "#000";
+    btn.style.backgroundColor = "#fff";
+    localStorage.setItem("ldark", "yes");
+    document.body.classList.add("darkmode");
+  } else {
+    btn.innerHTML = `<i class="fas fa-moon"></i>`;
+   btn.style.color = "orange";
+    btn.style.backgroundColor = "#111";
+    localStorage.removeItem("ldark");
+    document.body.classList.remove("darkmode");
+  }
+}
+
+
+
+
+
 
 

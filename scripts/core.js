d2a78081ee04d309338dba904f4f5923c5fe44a3
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-03-06 21:54:22 -0300

    Update core.js

--- scripts/core.js
+++ scripts/core.js
@@ -1,4 +1,26 @@
 
+function getUrlParameter(sParam) {  var dgetUrlParameterd="";
+
+    var sPageURL = decodeURIComponent(location.href);//window.location.search.substring(1));
+
+    var sURLVariables = sPageURL.split('?')[1].split('&');
+
+    for (var i = 0; i < sURLVariables.length; i++) {
+
+        var sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] == sParam) {
+
+            dgetUrlParameterd=sParameterName[1];
+
+        }
+
+    }
+
+return decodeURI(dgetUrlParameterd).replaceAll("+"," ");  }
+
+
+
 
 function mchangelag(nn) {
 if(nn){  location.href=nn;  }

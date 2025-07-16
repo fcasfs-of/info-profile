b0f1d4abbdc9c98d232f013954df0882d593aade
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-07-16 10:10:31 -0300

    Update code.js

--- scripts/code.js
+++ scripts/code.js
@@ -5,18 +5,6 @@ function mylangs(id) {
   omylangsngh.classList.toggle("show");
 }
 
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
 
 
 
